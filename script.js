7af4641f53f50280336066ad53a1608cb212cdd0
sophiachen634 <148411242+sophiachen634@users.noreply.github.com>
2024-04-25 16:39:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <title>Caramel Cake</title>
+    <link rel="stylesheet" href="recipeStyles.css">
+</head>
+
+<body>
+    <h1>Caramel Cake</h1>
+    <img src="https://www.allrecipes.com/thmb/cRtZNweGDnsN5Yqt9Yzr_l44IDk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7707902-0ad180221b844e63a82d4fd81db2c8c0.jpg" width="500px" alt="A picture of the finished cake">
+    <p class="description">Moist caramel cake with delicious layers of cake and caramel icing.</p>
+
+    <h2>Ingredients</h2>
+
+        <h3>Cake</h3>
+            <li>1 3/4 cup whole milk</li>
+            <li>2 tbsp white sugar</li>
+            <li>1 tbsp honey</li>
+            <li>3 Lipton black tea bags</li>
+            <li>1 package (1 tbsp) gelatin</li>
+            <li>1/2 cup dried boba/tapioca balls</li>
+            <li>3 cups of water</li>
+            <li>1/4 cup brown sugar</li>
+
+
+        <h2>Directions For Creating The Boba Pudding, Step by Step</h2>
+        <ol>
+            <li>To make boba: In a medium pot, boil 3 cups of water and 1/4 cup brown sugar. When it boils, add the boba and let simmer in medium heat for 3 mins (it should float). Switch heat off and let sit for another few minutes while you prepare pudding</li>
+            <li>In a small pot, mix milk, sugar, honey and gelatin. Add tea bags and heat until milk is bubbling at edges and everything is dissolved. Squeeze and remove tea bags. Allow to cool for a minutes</li>
+            <li>Strain milk tea pudding mixture and pour equally into small pudding cups. Add 1 tbsp of prepared boba. Chill in refrigerator for at least 1-2 hours</li>
+
+
+        </ol>
+<a href="index.html">Go back to homepage</a>
+</body>
+
+</html>
